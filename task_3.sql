d40bcfe1e4ac0c1a867481a4956f3ba21bdc2b6d
abderrahim <abdou0606e@gmail.com>
2024-07-25 15:29:52 +0100

    Add SQL script to list tables in alx_book_store

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+-- This script will list all tables in the specified database
+
+-- Use the database specified by the command-line argument
+USE alx_book_store;
+
+-- List all tables in the current database
+SHOW TABLES;
